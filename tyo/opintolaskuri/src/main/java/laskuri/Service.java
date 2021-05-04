1c9bdb50f52a6b0d7342023eeb3b134bfb6c7620
jsandberg <joel.sandberg@helsinki.fi>
2021-05-04 19:04:32 +0300

    pikkujuttu

--- tyo/opintolaskuri/src/main/java/laskuri/Service.java
+++ tyo/opintolaskuri/src/main/java/laskuri/Service.java
@@ -18,9 +18,18 @@ import dao.AccountDao;
 public class Service {
     private AccountDao dao;
     private account logged;
+    /**
+     * Konstruktori servicelle
+     * @param dao 
+     */
     public Service(AccountDao dao) {
         this.dao = dao;
     }
+    /**
+     * Kirjaa sisään
+     * @param name
+     * @return 
+     */
     public boolean login(String name) {
         account acc = dao.findName(name);
         if (acc == null) {
@@ -29,9 +38,16 @@ public class Service {
         logged = acc;
         return true;
     }
+    /**
+     * Tulostaa nyt kirjautuneen accountin
+     * @return 
+     */
     public account getLoggedAcc() {
         return logged;
     }
+    /**
+     * Kirjaa ulos
+     */
     public void logout() {
         try {
             dao.synToFile(dao.getAll());
@@ -40,6 +56,12 @@ public class Service {
             logged = null;
         }
     }
+    /**
+     * Oikeesti luo accountin muistiin
+     * @param name
+     * @param pass
+     * @return 
+     */
     public boolean crtAccount(String name, String pass) {
         if (dao.findName(name) != null) {
             return false;
